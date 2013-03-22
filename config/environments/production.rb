890351a6a2d6dff2e00c16f2fb7391daafc8991a
Brian Wagner <brianrwagner@gmail.com>
2013-03-21 22:15:21 -0700

    Add Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,14 @@ Omrails::Application.configure do
 
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Paperclip should use Amazon S3 on Heroku
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => "onemonthrails-brian",
+      :access_key_id => "AKIAJQE7N7EDVCA6FQ4Q",
+      :secret_access_key => "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"
+    }
+}
 end
